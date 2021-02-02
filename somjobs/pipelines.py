6b44588f6336e5c7335305867157104c6d09f504
Khalid Abdillahi <Abdillahi_Khalid@bradycorp.com>
2021-02-02 12:55:35 +0000

    added shortened url

--- somjobs/pipelines.py
+++ somjobs/pipelines.py
@@ -1,5 +1,15 @@
+import urllib
+import requests
+
+
 class SomJobsPipeline(object):
+    key = '58f9f7655ba7c89e1f0c4ded45d3511802dc8'
     def process_item(self, item, spider):
+        url = urllib.parse.quote(item['link'])
+        req = requests.get('http://cutt.ly/api/api.php?key={}&short={}'.format(self.key, url))
+        req = req.json()
+        print(req)
         item['posted_date'] = item['posted_date'].split(":")[1]
         item['expires'] = item['expires'].split(":")[1]
+        item['short_url'] = req['url']['shortLink']
         return item
