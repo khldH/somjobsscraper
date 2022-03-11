9e920c885a73e59273534f7cbca45b1fd13842ea
khalidh <khalidh@directferries.com>
2022-03-11 11:05:28 +0000

    add settings path to crawler process

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from scrapy.crawler import CrawlerProcess
 from somjobs.spiders.job import JobSpider
+from scrapy.utils.project import get_project_settings
+settings = get_project_settings()
+
 
 if __name__ == "__main__":
-    process = CrawlerProcess()
+    process = CrawlerProcess(settings=settings)
     process.crawl(JobSpider)
     process.start()
