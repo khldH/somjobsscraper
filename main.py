b2d3d5da2e0ce3a5c4049cd86731bc790a6a2b66
Khalid Harun <khalidh@directferries.local>
2022-08-09 16:40:01 +0100

    comment local_db

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from somjobs.run_spiders import run_all_spiders
 aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')  # crawler.settings['aws_access_key_id']
 aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 
-db_local = boto3.resource("dynamodb", endpoint_url="http://localhost:8000")
+# db_local = boto3.resource("dynamodb", endpoint_url="http://localhost:8000")
 db = boto3.resource('dynamodb',
                     region_name="eu-west-2",
                     aws_access_key_id=aws_access_key_id,
